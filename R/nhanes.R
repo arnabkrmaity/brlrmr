546c8e7c8e47b8ded0bb6fe8f616ea5c6c6667f9
arnabkrmaity <arnabkrmaity@gmail.com>
2016-11-26 17:04:51 -0600

    nhanes data

---
+++ R/nhanes.R
@@ -0,0 +1,12 @@
+#' Subset of original NHANES data used in \code{mice} package.
+#'
+#' A small data set with missing values.
+#'
+#' @format A data frame with 25 observations on the following 2 variables:
+#' \describe{
+#'   \item{hyp}{Hypertensive (0 = no, 1 = yes)}
+#'   \item{age2}{Age group (1 = 40-59, 0 = otherwise)}
+#'   \item{age3}{Age group (1 = 60+, 0 = otherwise)}
+#' }
+#' @source \url{http://www.diamondse.info/}
+"nhanes"
