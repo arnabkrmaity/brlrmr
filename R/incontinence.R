e0457ee6a5a7a22a3efb1f5d118b3b975493a0ac
arnabkrmaity <arnabkrmaity@gmail.com>
2016-11-28 14:58:19 -0600

    incontinence manual

--- R/incontinence.R
+++ R/incontinence.R
@@ -1,12 +1,13 @@
-#' Subset of original NHANES data used in \code{mice} package.
+#' Incontinence example.
 #'
-#' A small data set with missing values.
+#' A urinary incontinence study.
 #'
-#' @format A data frame with 25 observations on the following 2 variables:
+#' @format A data frame with 21 observations on the following 4 variables:
 #' \describe{
-#'   \item{hyp}{Hypertensive (0 = no, 1 = yes)}
-#'   \item{age2}{Age group (1 = 40-59, 0 = otherwise)}
-#'   \item{age3}{Age group (1 = 60+, 0 = otherwise)}
+#'   \item{y}{Response (1 = continent, 0 = otherwise)}
+#'   \item{\eqn{x_j}}{Lower urinary tract measure, j = 1, 2, 3}
 #' }
-#' @source \url{http://www.diamondse.info/}
+#' @source Heinze, G. (2006). A comparative investigation of methods for logistic
+#' regression with seperated or nearly separated data. \emph{Statistics in Medicine},
+#' 25:4216--4226.
 "nhanes"
