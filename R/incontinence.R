1c180406d5a8495be5267bf735007b752c38ce23
arnabkrmaity <arnabkrmaity@gmail.com>
2016-11-29 14:53:52 -0600

    Eibare hoyeche

--- R/incontinence.R
+++ R/incontinence.R
@@ -5,7 +5,9 @@
 #' @format A data frame with 21 observations on the following 4 variables:
 #' \describe{
 #'   \item{y}{Response (1 = continent, 0 = otherwise)}
-#'   \item{\eqn{x_j}}{Lower urinary tract measure, j = 1, 2, 3}
+#'   \item{x1}{Lower urinary tract measure}
+#'   \item{x2}{Lower urinary tract measure}
+#'   \item{x3}{Lower urinary tract measure}
 #' }
 #' @source Heinze, G. (2006). A comparative investigation of methods for logistic
 #' regression with seperated or nearly separated data. \emph{Statistics in Medicine},
